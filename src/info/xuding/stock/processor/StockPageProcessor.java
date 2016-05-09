2ad4d26c0fdf4e1120d67c32662e874d74dbb315
xuding <xuding@pinsmedical.com>
2016-05-09 17:50:49 +0800

    改用东方财富的龙虎榜

--- src/info/xuding/stock/processor/StockPageProcessor.java
+++ src/info/xuding/stock/processor/StockPageProcessor.java
@@ -1,23 +1,23 @@
 package info.xuding.stock.processor;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.http.client.HttpClient;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import info.xuding.stock.dao.StockDao;
 import info.xuding.stock.model.TopBill;
 import info.xuding.stock.utils.StockPriceUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.JsonPathSelector;
 import us.codecraft.webmagic.selector.Selectable;
 
 /**
@@ -27,64 +27,68 @@ import us.codecraft.webmagic.selector.Selectable;
 @Component
 public class StockPageProcessor implements PageProcessor {
 
-	private static final String DATA_URL = "(http://data.10jqka.com.cn/market/lhbcjmx/code/\\w+/)";
+    @Resource
+    private StockDao stockDao;
 
-	@Resource
-	private StockDao stockDao;
+    @Override
+    public void process(Page page) {
+        String jsonStr = page.getRawText();
+        if (jsonStr.startsWith("var ")) {
+            jsonStr = page.getRawText().substring(jsonStr.indexOf('=') + 1);
+            JSONObject jsonObject = JSON.parseObject(jsonStr);
+            JSONArray jsonArray = jsonObject.getJSONArray("data");
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject stockObject = (JSONObject) jsonArray.get(i);
+                page.addTargetRequest("http://data.eastmoney.com/stock/lhb," + stockObject.getString("Tdate") + ","
+                        + stockObject.getString("SCode") + ".html");
+            }
+        }
+        String stockInfo = page.getHtml().$("div.tit a").xpath("tidyText()").toString();
+        if (stockInfo == null) {
+            return;
+        }
+        String stockName = stockInfo.substring(0, stockInfo.indexOf('('));
+        String stockCode = stockInfo.substring(stockInfo.indexOf('(') + 1, stockInfo.length() - 1);
+        String stockDate = page.getHtml().$("div.left.con-br").xpath("tidyText()").toString().substring(0, 10);
+        String closingPrice = page.getHtml().$("div.data-tips")
+        Selectable table = page.getHtml().$(".default_tab stock-detail-tab").nodes().get(0);
+        List<Selectable> list = table.$("tbody > tr").nodes();
+        for (int i = 0; i < list.size(); i++) {
+            try {
+                Selectable selectable = list.get(i);
+                TopBill topBill = new TopBill(stockDate, stockName, stockCode, 0, 0, 0);
+                topBill.setOrganization(selectable.xpath("tr/td[2]/div[1]/a[2]/text()").toString());
+                if (topBill.getOrganization() == null) {
+                    continue;
+                }
+                topBill.setBuyAmount(Double.valueOf(selectable.xpath("tr/td[3]/text()").toString()));
+                topBill.setBuyPercent(selectable.xpath("tr/td[4]/text()").toString());
+                topBill.setSellAmount(Double.valueOf(selectable.xpath("tr/td[5]/text()").toString()));
+                topBill.setSellPercent(selectable.xpath("tr/td[6]/text()").toString());
+                topBill.setNetAmount(Double.valueOf(selectable.xpath("tr/td[7]/text()").toString()));
+//                topBill.setTurnover(Double.valueOf(selectable.xpath("tr/td[8]/text()").toString()));
+//                topBill.setPrice(StockPriceUtils.getStockPrice(stockCode));
+                stockDao.add(topBill);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public void process(Page page) {
-		String thisUrl = page.getUrl().toString();
-		String date = thisUrl.substring(thisUrl.indexOf("date/") + 5, thisUrl.indexOf("date/") + 15);
-		List<String> urlList = page.getHtml().$("#maintable > tbody > tr > td").links().regex(DATA_URL).all();
-		for (String str : urlList) {
-			str += "date/" + date + "/ajax/";
-			page.addTargetRequest(str);
-		}
-		String stockName = page.getHtml().$("div.stock_name h2 strong").xpath("tidyText()").toString();
-		if (stockName == null) {
-			return;
-		}
-		String stockCode = page.getHtml().$("#pageStockCode").xpath("tidyText()").toString();
-		String stockDate = page.getHtml().$("select.select option[selected]").xpath("tidyText()").toString();
-		Selectable table = page.getHtml().$(".m_table").nodes().get(0);
-		List<Selectable> list = table.$("tbody > tr").nodes();
-		for (int i = 0; i < list.size(); i++) {
-			try {
-				Selectable selectable = list.get(i);
-				TopBill topBill = new TopBill(stockDate, stockName, stockCode, 0, 0, 0);
-				topBill.setOrganization(selectable.xpath("/tr/td[2]/a/text()").toString());
-				if (topBill.getOrganization() == null) {
-					continue;
-				}
-				topBill.setBuyAmount(Double.valueOf(selectable.xpath("tr/td[3]/text()").toString()));
-				topBill.setBuyPercent(selectable.xpath("tr/td[4]/text()").toString());
-				topBill.setSellAmount(Double.valueOf(selectable.xpath("tr/td[5]/text()").toString()));
-				topBill.setSellPercent(selectable.xpath("tr/td[6]/text()").toString());
-				topBill.setNetAmount(Double.valueOf(selectable.xpath("tr/td[7]/text()").toString()));
-				topBill.setTurnover(Double.valueOf(selectable.xpath("tr/td[8]/text()").toString()));
-				topBill.setPrice(StockPriceUtils.getStockPrice(stockCode));
-				stockDao.add(topBill);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public Site getSite() {
+        return Site.me().setRetryTimes(3).setSleepTime(0);
+    }
 
-	@Override
-	public Site getSite() {
-		return Site.me().setRetryTimes(3).setSleepTime(0);
-	}
-
-	// public static void main(String[] args) {
-	// ApplicationContext applicationContext = new
-	// ClassPathXmlApplicationContext(
-	// "classpath:/spring/applicationContext*.xml");
-	// StockPageProcessor stockPageProcessor =
-	// applicationContext.getBean(StockPageProcessor.class);
-	// Spider.create(stockPageProcessor).addUrl("http://data.10jqka.com.cn/market/lhbcjmx/code/000780/date/2016-04-26/ajax/").thread(5)
-	// .run();
-	// }
+    // public static void main(String[] args) {
+    // ApplicationContext applicationContext = new
+    // ClassPathXmlApplicationContext(
+    // "classpath:/spring/applicationContext*.xml");
+    // StockPageProcessor stockPageProcessor =
+    // applicationContext.getBean(StockPageProcessor.class);
+    // Spider.create(stockPageProcessor).addUrl("http://data.10jqka.com.cn/market/lhbcjmx/code/000780/date/2016-04-26/ajax/").thread(5)
+    // .run();
+    // }
 
 }
